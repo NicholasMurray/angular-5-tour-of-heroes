6fdb4d3f9ee1d17efea332aca7815a13d40f4dde
Nicholas Murray <murray.nicholas@gmail.com>
2018-05-08 21:17:51 +0100

    feature: observable changes in hero component and service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -20,7 +20,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeros(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
